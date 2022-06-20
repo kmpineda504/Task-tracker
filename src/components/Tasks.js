a7ea7e593b2e3be270419743df201d9112b86790
kmpineda504 <kmpineda504@outlook.com>
2022-06-20 20:28:42 +0200

    Creating and pulling each of the tasks through different components

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,28 +1,10 @@
 import React from 'react'
-const tasks = [
-    {
-        id: 1,
-        text: 'Doctors appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Meeting at school',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Food shopping',
-        day: 'Feb 5th at 2:30pm',
-        reminder: false,
-    },
-]
-const Tasks = () => {
+import Task from './Task'
+
+const Tasks = ({ tasks }) => {
   return (
     <>
-      {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
+      {tasks.map((task) => (<Task key={task.id} task={task} />))}
     </>
   )
 }
